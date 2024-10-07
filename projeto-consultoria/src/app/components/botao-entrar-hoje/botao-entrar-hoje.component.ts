1e0b9695c1d142e01fecd1a0aeadc44bf081357d
Gabriel Vilela <gabrielvilelaportela@gmail.com>
2024-10-07 20:28:05 -0300

    Estilizando hero section

---
+++ projeto-consultoria/src/app/components/botao-entrar-hoje/botao-entrar-hoje.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-entrar-hoje',
+  templateUrl: './botao-entrar-hoje.component.html',
+  styleUrl: './botao-entrar-hoje.component.scss'
+})
+export class BotaoEntrarHojeComponent {
+
+}
